680f6ea0eae2e92693d4481c11d44050e568c875
tiktikimelbo7 <tiktikimelbo7@gmail.com>
2022-07-12 04:22:07 +0600

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -340,6 +340,13 @@ try:
 except KeyError:
     TAR_UNZIP_LIMIT = None
 
+try:
+    STORAGE_THRESHOLD = getConfig('STORAGE_THRESHOLD')
+    if len(STORAGE_THRESHOLD) == 0:
+        raise KeyError
+    STORAGE_THRESHOLD = float(STORAGE_THRESHOLD)
+except:
+    STORAGE_THRESHOLD = None
 
 
 
